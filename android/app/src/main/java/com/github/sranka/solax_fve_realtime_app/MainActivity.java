bc2a9872030863ae2f81e77ee28f56e55d7ccdc1
Pavel Zavora <pavel.zavora@gmail.com>
2026-03-26 14:57:18 +0100

    feat: back activity causes ESC in modals

--- android/app/src/main/java/com/github/sranka/solax_fve_realtime_app/MainActivity.java
+++ android/app/src/main/java/com/github/sranka/solax_fve_realtime_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.github.sranka.solax_fve_realtime_app;
 
 import android.os.Bundle;
+import androidx.activity.OnBackPressedCallback;
 import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
@@ -10,5 +11,26 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(ModbusTcpPlugin.class);
         super.onCreate(savedInstanceState);
         WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+
+        // Android TV: let JS close modals on back press; exit app only if no modal is open
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (getBridge() == null || getBridge().getWebView() == null) {
+                    finish();
+                    return;
+                }
+                getBridge().getWebView().evaluateJavascript(
+                    "(function(){if(typeof _focusStack!=='undefined'&&_focusStack.length>0){" +
+                    "var e=new KeyboardEvent('keydown',{key:'Escape',bubbles:true});document.dispatchEvent(e);return true;" +
+                    "}return false;})()",
+                    result -> {
+                        if (!"true".equals(result)) {
+                            finish();
+                        }
+                    }
+                );
+            }
+        });
     }
 }
